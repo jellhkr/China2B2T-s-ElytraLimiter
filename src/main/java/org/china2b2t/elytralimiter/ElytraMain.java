2cc46acdc7a3b9994a0c697421276acece0c7ad2
jellhkr-star <63093775+jellhkr@users.noreply.github.com>
2020-12-06 09:28:16 +0800

    Update ElytraMain.java

--- src/main/java/org/china2b2t/elytralimiter/ElytraMain.java
+++ src/main/java/org/china2b2t/elytralimiter/ElytraMain.java
@@ -21,11 +21,13 @@ public class ElytraMain extends JavaPlugin implements Listener{
     public void onPlayerMove(PlayerMoveEvent event) {
         if(event.getPlayer().isGliding()) {
             Vector x = event.getTo().toVector();
-            Vector y = event.getFrom().toVector();
+            Vector y = event.getFrom().toVector();//计算航线
+
+
 
             Vector z = x.subtract(y);
             if((Math.abs(z.getX()) > 1 || Math.abs(z.getZ()) > 1) && z.getY() == 0) {
-                event.setCancelled(true);
+                event.setCancelled(true);//如果符合拉回条件，则拉回
             }
         }
     }
